1eca53a276c4dabd805b4e0ba87b1eb944a2820a
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 18:26:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,3 +6,7 @@ var playlist = {
 function updatePlaylist(playlist, artistName, songTitle) {
   return playlist["Phil Ochs"] = "Here's to the State of Mississippi"
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  return delete playlist.artistName
+}
